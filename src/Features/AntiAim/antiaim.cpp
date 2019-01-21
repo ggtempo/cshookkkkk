8a6a4314f921c69f2e57f777451abcdce272b033
dminikm <dminik.m@email.cz>
2019-01-21 01:04:06 +0100

    Added more anti-aim options, better menu

--- src/Features/AntiAim/antiaim.cpp
+++ src/Features/AntiAim/antiaim.cpp
@@ -7,11 +7,59 @@ namespace features
     {
         if (this->enabled && !(cmd->buttons & IN_ATTACK))
         {
-            static float angle = 0.0f;
-
+            static float angle = 0.0;
             auto view = cmd->viewangles;
-            auto new_view = math::vec3{-89.0f, angle, 0.0f};
+            auto new_view = view;
             auto move = vec3_t{cmd->forwardmove, cmd->sidemove, cmd->upmove};
+
+            switch (this->pitch_mode)
+            {
+                case aa_mode_pitch::down_emotion:
+                    new_view.x = -89.0f;
+                    break;
+
+                case aa_mode_pitch::down_unsafe:
+                    new_view.x = -179.0f;
+                    move.x *= -1;
+                    break;
+
+                case aa_mode_pitch::up_emotion:
+                    new_view.x = 89.0f;
+                    break;
+
+                case aa_mode_pitch::up_unsafe:
+                    new_view.x = 179.0f;
+                    move.x *= -1;
+                    break;
+            }
+
+            switch (this->yaw_mode)
+            {
+                case aa_mode_yaw::backwards:
+                    new_view.y -= 180.0f;
+                    break;
+                
+                case aa_mode_yaw::left:
+                    new_view.y += 90.0f;
+                    break;
+
+                case aa_mode_yaw::right:
+                    new_view.y -= 90.0f;
+                    break;
+
+                case aa_mode_yaw::spin:
+                    new_view.y = angle;
+                    angle += 20;
+                    break;
+            }
+
+            while (angle >= 360.0f)
+                angle -= 360.0f;
+
+            while (angle < 0.0f)
+                angle += 360.0f;
+
+            
             cmd->viewangles = new_view;
 
             auto new_move = math::correct_movement(view, new_view, move);
@@ -26,27 +74,71 @@ namespace features
             cmd->sidemove = new_move.y;
             cmd->upmove = new_move.z;
 
-            while (cmd->viewangles.y > 360)
-            {
-                cmd->viewangles.y -= 360;
-                angle -= 360;
-            }
-
-            while (cmd->viewangles.y < 0)
-            {
-                cmd->viewangles.y += 360;
-                angle += 360;
-            }
-
             cmd->viewangles.z = 0;
-
-            angle += 50;
         }
     }
 
     void anti_aim::show_menu()
     {
+        struct option
+        {
+            const char* name;
+            const char* tooltip;
+        };
+
+        const static std::vector<option> pitch_modes = {
+            {"Off", "Default pitch"},
+            {"Down (emotion)", "Pitch is set to -89"},
+            {"Down (unsafe)", "Pitch is set to -179 (outside the normal range)"},
+            {"Up (emotion)", "Pitch is set to 89"},
+            {"Up (unsafe)", "Pitch is set to 179 (outside the normal range)"}
+        };
+
+        const static std::vector<option> yaw_modes = {
+            {"Off", "Default yaw"},
+            {"Backwards", "Yaw is offset by 180 (Player is walking backwards)"},
+            {"Left", "Yaw is offset by +90 (Player is walking sideways)"},
+            {"Right", "Yaw is offset by -90 (Player is walking sideways)"},
+            {"Spin", "Yaw is spinning"}
+        };
+
         ImGui::Begin("Anti-Aim");
+            ImGui::Checkbox("Enabled", &this->enabled);
+            
+            if (ImGui::BeginCombo("Pitch mode", pitch_modes[(int)this->pitch_mode].name))
+            {
+                for (size_t i = 0; i < pitch_modes.size(); i++)
+                {
+                    bool selected = (i == (size_t)this->pitch_mode);
+                    if (ImGui::Selectable(pitch_modes[i].name, selected))
+                        this->pitch_mode = (aa_mode_pitch)i;    // If we selected this mode, remember it
+
+                    if (ImGui::IsItemActive() || ImGui::IsItemHovered())
+                        ImGui::SetTooltip(pitch_modes[i].tooltip);  // If we are hovering over the option, display it's tooltip
+
+                    if (select)
+                        ImGui::SetItemDefaultFocus();   // Scroll to the currently selected otion
+                }
+                ImGui::EndCombo();
+            }
+
+            if (ImGui::BeginCombo("Yaw mode", yaw_modes[(int)this->yaw_mode].name))
+            {
+                for (size_t i = 0; i < yaw_modes.size(); i++)
+                {
+                    bool selected = (i == (size_t)this->yaw_mode);
+
+                    if (ImGui::Selectable(yaw_modes[i].name, selected))
+                        this->yaw_mode = (aa_mode_yaw)i;    // If we selected this mode, remember it
+
+                    if (ImGui::IsItemActive() || ImGui::IsItemHovered())
+                        ImGui::SetTooltip(yaw_modes[i].tooltip);  // If we are hovering over the option, display it's tooltip
+
+                    if (select)
+                        ImGui::SetItemDefaultFocus();   // Scroll to the currently selected otion
+                }
+                ImGui::EndCombo();
+            }
         ImGui::End();
     }
 }
--- src/Features/AntiAim/antiaim.hpp
+++ src/Features/AntiAim/antiaim.hpp
@@ -1,14 +1,52 @@
 #pragma once
 #include "../../Utils/globals.hpp"
 
+/*
+    Anti aim modes
+    Pitch:
+        Down (emotion)  -> 89
+        Down (unsafe)   -> 179
+        Up (emotion)    -> -89
+        Up (unsafe)     -> -179
+
+
+    Yaw:
+        Backwards       -> Yaw + 180
+        Left            -> Yaw + 90
+        Right           -> Yaw - 90
+        Spin            -> Angle + spin speed
+
+
+*/
+
 namespace features
 {
+    enum class aa_mode_pitch
+    {
+        off,
+        down_emotion,
+        down_unsafe,
+        up_emotion,
+        up_unsafe
+    };
+
+    enum class aa_mode_yaw
+    {
+        off,
+        backwards,
+        left,
+        right,
+        spin
+    };
+
     class anti_aim
     {
         private:
             anti_aim()
             {
                 this->enabled = false;
+                this->pitch_mode = aa_mode_pitch::off;
+                this->yaw_mode = aa_mode_yaw::off;
             }
         
         public:
@@ -22,6 +60,8 @@ namespace features
             void show_menu();
 
         private:
-            bool enabled;
+            bool            enabled;
+            aa_mode_pitch   pitch_mode;
+            aa_mode_yaw     yaw_mode;
     };
 }
