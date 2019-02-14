e4496a9d32db8fd236a9f27b74f60e53314b85f9
dminikm <dminik.m@email.cz>
2019-02-14 18:57:16 +0100

    Converted indentation to spaces (again)

--- src/Utils/utils.hpp
+++ src/Utils/utils.hpp
@@ -9,12 +9,12 @@
 
 namespace utils
 {
-	struct module_info
-	{
-		HINSTANCE instance;
-		uint32_t reason;
-		uint32_t pid;
-	};
+    struct module_info
+    {
+        HINSTANCE instance;
+        uint32_t reason;
+        uint32_t pid;
+    };
 
     BOOL CALLBACK enum_proc_callback(HWND hwnd, LPARAM lparam)
     {
@@ -33,50 +33,50 @@ namespace utils
         return FALSE;
     }
 
-	inline HWND get_main_window(uint32_t pid)
-	{
-		std::pair<uint32_t, std::vector<HWND>> data;
-		data.first = pid;
+    inline HWND get_main_window(uint32_t pid)
+    {
+        std::pair<uint32_t, std::vector<HWND>> data;
+        data.first = pid;
 
-		EnumWindows(enum_proc_callback, (LPARAM)&data);
+        EnumWindows(enum_proc_callback, (LPARAM)&data);
 
-		// Just search for the largest window ...
-		HWND largest_handle = 0;
-		int32_t largest_size = 0;
+        // Just search for the largest window ...
+        HWND largest_handle = 0;
+        int32_t largest_size = 0;
 
-		for (auto window : data.second)
-		{
-			RECT r = {};
-			GetWindowRect(window, &r);
+        for (auto window : data.second)
+        {
+            RECT r = {};
+            GetWindowRect(window, &r);
 
-			int32_t size = r.right * r.bottom;
-			if (size > largest_size)
-			{
-				largest_handle = window;
-				largest_size = size;
-			}
-		}
+            int32_t size = r.right * r.bottom;
+            if (size > largest_size)
+            {
+                largest_handle = window;
+                largest_size = size;
+            }
+        }
 
-		if (!IsWindow(largest_handle))
-		{
-			auto i = 5;
-		}
+        if (!IsWindow(largest_handle))
+        {
+            auto i = 5;
+        }
 
-		return largest_handle;
-	}
-	
-	inline std::optional<std::wstring> get_base_path(HMODULE hModule)
-	{
-		wchar_t buf[MAX_PATH];
-		if (auto result = GetModuleFileName(hModule, buf, sizeof(wchar_t) * MAX_PATH); result > 0)
-		{
-			auto baseName = std::wstring(buf);
-			auto last = baseName.find_last_of(L"\\");
-			return baseName.substr(0, last + 1);
-		}
-		else
-		{
-			return std::nullopt;
-		}
-	}
+        return largest_handle;
+    }
+    
+    inline std::optional<std::wstring> get_base_path(HMODULE hModule)
+    {
+        wchar_t buf[MAX_PATH];
+        if (auto result = GetModuleFileName(hModule, buf, sizeof(wchar_t) * MAX_PATH); result > 0)
+        {
+            auto baseName = std::wstring(buf);
+            auto last = baseName.find_last_of(L"\\");
+            return baseName.substr(0, last + 1);
+        }
+        else
+        {
+            return std::nullopt;
+        }
+    }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -27,8 +27,8 @@ void ThreadMain()
 {
     auto& g = globals::instance();
 
-	auto parentProcess = GetModuleHandle(NULL);
-	g.main_window = utils::get_main_window(module_info->pid);
+    auto parentProcess = GetModuleHandle(NULL);
+    g.main_window = utils::get_main_window(module_info->pid);
     auto base_path = utils::get_base_path(module_info->instance);
     auto full_path = ((base_path) ? *base_path : std::wstring(L"%USERPROFILE%")) + std::wstring(L"\\CSHook.ini");
     std::string base_path_str = std::wstring_convert<std::codecvt_utf8<wchar_t>>().to_bytes(full_path);
@@ -40,29 +40,29 @@ void ThreadMain()
 
 BOOL WINAPI DllMain(HINSTANCE instance, DWORD reason, LPVOID reserved)
 {
-	if (reason == DLL_PROCESS_ATTACH)
-	{
-		DisableThreadLibraryCalls(instance);
+    if (reason == DLL_PROCESS_ATTACH)
+    {
+        DisableThreadLibraryCalls(instance);
 
-		/*if (auto basePath = Utils::GetBasePath(hInstance); basePath)
-		{
-			SetDllDirectory((*basePath).c_str());
-		}*/
+        /*if (auto basePath = Utils::GetBasePath(hInstance); basePath)
+        {
+            SetDllDirectory((*basePath).c_str());
+        }*/
 
-		auto pid = GetCurrentProcessId();
+        auto pid = GetCurrentProcessId();
 
-		module_info = new utils::module_info();
-		module_info->reason = reason;
-		module_info->instance = instance;
-		module_info->pid = pid;
+        module_info = new utils::module_info();
+        module_info->reason = reason;
+        module_info->instance = instance;
+        module_info->pid = pid;
 
 
-		CreateThread(NULL, NULL, reinterpret_cast<LPTHREAD_START_ROUTINE>(&ThreadMain), nullptr, NULL, NULL);
+        CreateThread(NULL, NULL, reinterpret_cast<LPTHREAD_START_ROUTINE>(&ThreadMain), nullptr, NULL, NULL);
 
-		return true;
-	}
-	else
-	{
-		return true;
-	}
+        return true;
+    }
+    else
+    {
+        return true;
+    }
 }
